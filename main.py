fdba5a7737c57aff5c300702f927c389d612f5a6
Arbaaz <khanarbaj45@gmail.com>
2020-11-28 20:58:54 +0530

    Feat : Convert color image into grayscale

--- main.py
+++ main.py
@@ -7,4 +7,5 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # Read image file
 color_image = cv2.imread('Mypic.jpg')
 
-
+# Convert color image into grayscale
+gray_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
