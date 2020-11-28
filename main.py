4a60294bff8abb74cddd428fb9361712674bae28
Arbaaz <khanarbaj45@gmail.com>
2020-11-28 21:00:16 +0530

    Feat : Detect faces ROI

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ color_image = cv2.imread('Mypic.jpg')
 
 # Convert color image into grayscale
 gray_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
+
+# Detect faces ROI
+#Syntax: Classifier.detectMultiScale(input image, Scale Factor , Min Neighbors)
+faces = face_cascade.detectMultiScale(gray_image, 1.1, 5)
+
+
