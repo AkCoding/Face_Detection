20a1df131242470c73b66c42c656bed88592fd22
Arbaaz <khanarbaj45@gmail.com>
2020-11-28 21:02:15 +0530

    Feat : Show Image

--- main.py
+++ main.py
@@ -17,3 +17,12 @@ faces = face_cascade.detectMultiScale(gray_image, 1.1, 5)
 # Draw rectangle around the faces
 for (x, y, w, h) in faces:
     cv2.rectangle(color_image, (x, y), (x + w, y + h), (0, 0, 255), 4)
+
+# Show image
+cv2.imshow('Image', color_image)
+
+# wait to close window
+cv2.waitKey()
+
+# close all windows
+cv2.destroyAllWindows()
