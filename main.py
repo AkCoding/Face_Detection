0086d3507ee58333fafa2037a001358ad4a202de
Arbaaz <khanarbaj45@gmail.com>
2020-11-28 20:55:21 +0530

    Feat : Read the given image

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import cv2
 
 # Load trained cascade classifier
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# Read image file
+color_image = cv2.imread('girl.png')
+
+
