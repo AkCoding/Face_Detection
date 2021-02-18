34a4a361b497684081cfddfded7be31cd66f893c
arbaj <khanarbaj45@gmail.com>
2021-02-18 12:16:50 +0530

    Feat : Added files

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ faces = face_cascade.detectMultiScale(gray_image, 1.1, 5)
 
 # Draw rectangle around the faces
 for (x, y, w, h) in faces:
-    cv2.rectangle(color_image, (x, y), (x + w, y + h), (0, 0, 255), 4)
+    cv2.rectangle(color_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
 
 # Show image
 cv2.imshow('Image', color_image)
